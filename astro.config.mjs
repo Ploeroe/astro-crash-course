2d03f5dc18b63a8c3e7bb5c4fac8779995861d51
Ploeroe <raphael.hakim@student.umn.ac.id>
2024-09-11 02:13:36 +0700

    Added ssr and deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,5 +13,5 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: 'server',
-  adapter: vercel()
+  adapter: netlify()
 });
